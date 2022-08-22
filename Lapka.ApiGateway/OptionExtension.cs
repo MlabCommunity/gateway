f36126d085b4077f7681307f043a0707ac2bd017
Madziks <majta.magdalena@gmail.com>
2022-08-22 11:27:06 +0200

    ApiGateway

---
+++ Lapka.ApiGateway/OptionExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Lapka.ApiGateway;
+
+public static class OptionExtension
+{
+    public static T GetProjectOptions<T>(this IConfiguration configuration, string sectionName) where T : new()
+    {
+        var options = new T();
+        configuration.GetSection(sectionName).Bind(options);
+        return options;
+    }
+}
---
+++ Lapka.ApiGateway/Swagger/SwaggerExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace Lapka.ApiGateway.Swagger;
+
+public static class SwaggerExtension
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen();
+        services.AddEndpointsApiExplorer();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseSwaggerDocs(this IApplicationBuilder app, List<SwaggerEndpoint> endpoints)
+    {
+        return app
+            .UseSwagger()
+            .UseSwaggerUI(c =>
+            {
+                foreach (var e in endpoints)
+                {
+                    c.SwaggerEndpoint(e.GetFullUrl(), e.GetFullName());
+                }
+            });
+    }
+}
